27ed0aaa751ad2b8c4c835b5a06ebbf32677038b
Pericles Telemachou <pericles@outlook.com.au>
2022-11-22 18:38:21 +1100

    'Front-load' user input prompts

--- main.sh
+++ main.sh
@@ -1,5 +1,67 @@
-echo "Started automatic Arch Linux installer
+echo "Started automated Arch Linux installer
 "
+
+# 1. Prompt user for each required environment variable (if not already set)
+# 1.1. Get WiFi credentials (OPTIONAL)
+echo "Do you want to setup a wi-fi connection (for the installation)? [Y/n]"
+read -r USE_WIFI
+if [ "$USE_WIFI" = "Y" || "$USE_WIFI" = "y" ]
+then
+	if [ "$WIFI_SID" = "" ]
+	then
+		echo "Please specify your WIFI SID below:"
+		while [ "$WIFI_SID" = "" ]
+			read -r SID
+			export "WIFI_SID=$SID"
+			if [ "$WIFI_SID" = "" ]
+				echo "No value provided for wifi sid, please specify:"
+			fi
+	fi
+
+	if [ "$WIFI_PASS" = "" ]
+	then
+		# TODO: This section is repeated, figure out how to write POSIX shell functions
+		echo "Please specify your WIFI password below:"
+		while [ "$WIFI_PASS" = "" ]
+			read -r PASS
+			export "WIFI_SID=$PASS"
+			if [ "$WIFI_PASS" = "" ]
+				echo "No value provided for wifi password, please specify:"
+			fi
+	fi
+fi
+
+# 1.2. Get the target drive
+if [ "$TARGET_DRIVE" = "" ]
+then
+	echo " Which disk would you like to install to (e.g. 'sda', 'sdb', 'nvme0', etc
+	WARNING: This disk will be wiped:"
+	lsblk | grep disk | while read j; do	
+		id=$(echo $j | awk '{print $1}')
+		size=$(echo $j | awk '{print $4}')
+		model=$(cat /sys/class/block/"$id"/device/model)
+		echo " - $id ($size): $model"
+	done 
+
+	TARGET_VALID=0
+	while [ $TARGET_VALID == 0 ]; do
+		read -r TARGET_DRIVE
+		TARGET_VALID=$(fdisk -l /dev/$TARGET_DRIVE 2>&1 | grep -v 'cannot' | wc -l)
+		if [ $TARGET_VALID == 0 ]
+		then
+			echo " Invalid disk: Type an id from the list above should look similar to 'sdX' or 'nvmeX'"
+		fi
+	done 
+fi
+
+# 1.3. Get the user's desired partition scheme
+# LVM?
+# Encryption?
+# Separate /home and /
+
+# 1.4. Get hardware specific drivers
+
+# 1.5. Ask for how large they'd like swap to be
 
 # 0. Inform user of environment variables to set
 echo "STEP 1: Environment Variables Setup"
@@ -44,26 +106,7 @@ fi
 
 # 2. Partition disk for boot, root (lvm and encryption?)
 echo "STEP 3: Install disk selection (only 1 disk per installation supported)"
-if [ "$TARGET_DRIVE" = "" ]
-then
-	echo " Which disk would you like to install to (WARNING: This disk will be wiped):"
-	lsblk | grep disk | while read j; do	
-		id=$(echo $j | awk '{print $1}')
-		size=$(echo $j | awk '{print $4}')
-		model=$(cat /sys/class/block/"$id"/device/model)
-		echo " - $id ($size): $model"
-	done 
 
-	TARGET_VALID=0
-	while [ $TARGET_VALID == 0 ]; do
-		read -r TARGET_DRIVE
-		TARGET_VALID=$(fdisk -l /dev/$TARGET_DRIVE 2>&1 | grep -v 'cannot' | wc -l)
-		if [ $TARGET_VALID == 0 ]
-		then
-			echo " Invalid disk: Type an id from the list above should look similar to 'sdX' or 'nvmeX'"
-		fi
-	done 
-fi
 echo $TARGET_DRIVE
 # 3. Generate the `fstab` file
 
