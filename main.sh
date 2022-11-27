7226d51c562d1238daa7c76aeb344bc5ba56f688
Pericles Telemachou <pericles@outlook.com.au>
2022-11-27 15:04:09 +1100

    Start on partitioning prompts, define 'partition scheme' regex

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "## Started automated Arch Linux installer ##
 "
 
@@ -42,58 +43,9 @@ if [ "$TARGET_DRIVE" = "" ]
 then
 	echo ""
 	echo "1.2: Which disk would you like to install to (e.g. 'sda', 'sdb', 'nvme0', etc):"
-	lsblk | grep disk | while read j; do	
-		id=$(echo $j | awk '{print $1}')
-		size=$(echo $j | awk '{print $4}')
 
-		# Used
-		diskInfo=$(fdisk -l /dev/"$id")
-		totalSizeBytes=$(echo $diskInfo | head -n 1 | awk '{print $5}')
-		usedSizeBytes=0
-		capacityUsed=$(fdisk -l /dev/$id | grep "^/dev/$id[\d]*" | awk '{print $5}' | while read k; do
-			partSizeBytes=0
-			# Num is rounded to NEAREST integer
-			num=$(echo $k | sed 's/.$//' | awk '{print int($1+0.5)}')
-			
-			if [ $(echo "${k: -1}") = "B" ];
-			then
-				partSizeBytes=$num
-			elif [ $(echo "${k: -1}") = "K" ];
-			then
-				partSizeBytes=$((num*1024))
-			elif [ $(echo "${k: -1}") = "M" ];
-			then
-				partSizeBytes=$((num*1024*1024))
-			elif [ $(echo "${k: -1}") = "G" ];
-			then
-				partSizeBytes=$((num*1024*1024*1024))
-			elif [ $(echo "${k: -1}") = "T" ];
-			then
-				partSizeBytes=$((num*1024*1024*1024*1024))
-			elif [ $(echo "${k: -1}") = "P" ];
-			then
-				partSizeBytes=$((num*1024*1024*1024*1024*1024))
-			else
-				echo "Sorry we can't handle EXABYTES of storage..."
-				exit 1
-			fi
-			usedSizeBytes=$((usedSizeBytes+partSizeBytes))
-			pcFree=$(awk "BEGIN {print int((($usedSizeBytes/$totalSizeBytes)*100)+0.5)}")
-			if [ $pcFree -gt 100 ];
-			then
-				pcFree=100
-			fi
-			echo $pcFree
-		done | tail -n 1)
-		model=$(cat /sys/class/block/"$id"/device/model | tr -d ' ')
-		isUSB=$(find /dev/disk/by-id/ -lname "*""$id" | grep usb | wc -l)
-		if [ $isUSB -gt 0 ];
-		then 
-			echo "* $id ($size, $capacityUsed% USED): $model (USB)"
-		else
-			echo "* $id ($size, $capacityUsed% USED): $model"
-		fi
-	done 
+	# Show user the available disks, with additional info to help them identify the right one
+	./components/disk-options
 
 	TARGET_VALID=0
 	while [ $TARGET_VALID == 0 ]; do
@@ -106,6 +58,78 @@ then
 	done 
 fi
 
+# 1.3 Get the partition scheme
+# Use a string to define paritioning:
+# e.g. 1 1 1 / 30% /boot 768M 
+# Args as follows:
+# * WHOLE/REMAINING(0,1)
+# * USE_LVM(0,1)
+# * USE_ENCRYPTION(0,1)
+# * '/' (fat|ext3|ext4) (1 -> 100% OR nM OR nG) ext4/ext3/zfs/etc
+# * '/boot' (fat) (1 -> 10% OR nM) fat
+# * optionally more partitions...
+PART_VALID=0
+if [ "$PARTITION_SCHEME" != "" ];
+then
+    requiredRx="[0,1]\s[0,1]\s[0,1]\s(\/)\s(fat|ext4|ext3)\s([1-9][0-9]?%|100%|\d{3}[M,G])\s(\/boot)\s(fat)\s([1-9]?%|\d{3}[M])"
+	optionalPartitionsRx="((\s)(\/([a-z]{1,20}))+(\s)(fat|ext4|ext3)\s([1-9][0-9]?%|100%|\d{3}[M,G]))"
+	rx="^""$requiredRx""$optionalPartitionsRx*""$"
+	PART_VALID=$(echo "$PARTITION_SCHEME" | grep \'$rx\' | wc -l)
+fi
+
+# If the partition scheme is not valid OR not specified we must ask all partitioning related questions
+if [ $PART_VALID = 0 ];
+then
+	WHOLE_REMAINING=2
+	while [ $WHOLE_REMAINING != 0 ] && [ $WHOLE_REMAINING != 1 ]; do
+		echo "1.3.1 Would you like to wipe the WHOLE drive or just use REMAINING capacity?
+		(0) WHOLE drive
+		(1) REMAINING capacity"
+		read -p "> " WHOLE_REMAINING
+		if [ $WHOLE_REMAINING != 0 ] && [ $WHOLE_REMAINING != 1 ]; then
+			echo " Invalid input: You must provide either \"0\" or \"1\""
+		fi
+	done
+
+	USE_LVM=2
+	while [ $USE_LVM != 0 ] && [ $USE_LVM != 1 ]; do
+		echo "1.3.2 Would you like to use LVM for partitioning (alternative is standard partitioning)?
+		(0) Use standard partitioning
+		(1) Use LVM"
+		read -p "> " USE_LVM
+		if [ $USE_LVM != 0 ] && [ $USE_LVM != 1 ]; then
+			echo " Invalid input: You must provide either \"0\" or \"1\""
+		fi
+	done
+
+	USE_ENCRYPTION=2
+	while [ $USE_ENCRYPTION != 0 ] && [ $USE_ENCRYPTION != 1 ]; do
+		echo "1.3.3 Would you like to encrypt your non-boot partitions/LV?
+		(0) DON'T encrypt partitions
+		(1) Encrypt partitions"
+		read -p "> " USE_ENCRYPTION
+		if [ $USE_ENCRYPTION != 0 ] && [ $USE_ENCRYPTION != 1 ]; then
+			echo " Invalid input: You must provide either \"0\" or \"1\""
+		fi
+	done
+
+	
+else
+	# Deconstruct partition scheme into arguments
+
+fi
+
+
+# 4. Partitions for which directories, how many
+# 	Minimally need root but can have more
+#	4.1. Keep prompting the user for more partitions
+partitionStr=""
+ROOT_SPECIFIED=0
+while [ $ROOT_SPECIFIED == 0 ]; do
+	echo "Please specify how much"
+	read -p "> " ROOT_SIZE
+done
+
 # 1.3. Get the user's desired partition scheme
 # LVM?
 # Encryption?
@@ -114,6 +138,23 @@ fi
 # 1.4. Get hardware specific drivers
 
 # 1.5. Ask for how large they'd like swap to be
+if [ "$SWAP_SIZE" = "" ]
+then
+	echo "1.5.1 Please specify the size of your SWAP partition below (e.g. 20G, 300M, etc):"
+
+	# Is swap greater than remaining space? Not valid
+	# Is swap greater than RAM? Ask if they're sure
+
+	SWAP_VALID=0
+	while [ $SWAP_VALID == 0 ]; do
+		read -p "> " SWAP_SIZE
+		TARGET_VALID=$(fdisk -l /dev/$TARGET_DRIVE 2>&1 | grep -v 'cannot' | wc -l)
+		if [ $TARGET_VALID == 0 ]
+		then
+			echo " Invalid disk: Type an id from the list above should look similar to 'sdX' or 'nvmeX'"
+		fi
+	done
+fi
 
 # 2. Check network connection (ask if wifi is available, otherwise ethernet should auto connect)
 echo "
