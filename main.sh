ea98e83d1634620557e949eaecca279fe50250c0
Pericles Telemachou <pericles@outlook.com.au>
2022-11-27 04:14:57 +0000

    Minor fixes, after testing on Arch

--- main.sh
+++ main.sh
@@ -45,7 +45,7 @@ then
 	echo "1.2: Which disk would you like to install to (e.g. 'sda', 'sdb', 'nvme0', etc):"
 
 	# Show user the available disks, with additional info to help them identify the right one
-	./components/disk-options
+	./components/disk-options.sh
 
 	TARGET_VALID=0
 	while [ $TARGET_VALID == 0 ]; do
@@ -69,54 +69,56 @@ fi
 # * '/boot' (fat) (1 -> 10% OR nM) fat
 # * optionally more partitions...
 PART_VALID=0
-if [ "$PARTITION_SCHEME" != "" ];
+if [ "$PARTITION_SCHEME" != "" ]
 then
-    requiredRx="[0,1]\s[0,1]\s[0,1]\s(\/)\s(fat|ext4|ext3)\s([1-9][0-9]?%|100%|\d{3}[M,G])\s(\/boot)\s(fat)\s([1-9]?%|\d{3}[M])"
+	requiredRx="[0,1]\s[0,1]\s[0,1]\s(\/)\s(fat|ext4|ext3)\s([1-9][0-9]?%|100%|\d{3}[M,G])\s(\/boot)\s(fat)\s([1-9]?%|\d{3}[M])"
 	optionalPartitionsRx="((\s)(\/([a-z]{1,20}))+(\s)(fat|ext4|ext3)\s([1-9][0-9]?%|100%|\d{3}[M,G]))"
 	rx="^""$requiredRx""$optionalPartitionsRx*""$"
 	PART_VALID=$(echo "$PARTITION_SCHEME" | grep \'$rx\' | wc -l)
 fi
 
 # If the partition scheme is not valid OR not specified we must ask all partitioning related questions
-if [ $PART_VALID = 0 ];
+if [ $PART_VALID == 0 ]
 then
+	echo ""
 	WHOLE_REMAINING=2
 	while [ $WHOLE_REMAINING != 0 ] && [ $WHOLE_REMAINING != 1 ]; do
-		echo "1.3.1 Would you like to wipe the WHOLE drive or just use REMAINING capacity?
-		(0) WHOLE drive
-		(1) REMAINING capacity"
+		echo "1.3.1 Would you like to wipe the WHOLE drive or just use REMAINING capacity?"
+		echo "(0) WHOLE drive"
+		echo "(1) REMAINING capacity"
 		read -p "> " WHOLE_REMAINING
-		if [ $WHOLE_REMAINING != 0 ] && [ $WHOLE_REMAINING != 1 ]; then
+		if [ $WHOLE_REMAINING != 0 ] && [ $WHOLE_REMAINING != 1 ]
+		then
 			echo " Invalid input: You must provide either \"0\" or \"1\""
 		fi
 	done
 
 	USE_LVM=2
 	while [ $USE_LVM != 0 ] && [ $USE_LVM != 1 ]; do
-		echo "1.3.2 Would you like to use LVM for partitioning (alternative is standard partitioning)?
-		(0) Use standard partitioning
-		(1) Use LVM"
+		echo "1.3.2 Would you like to use LVM for partitioning (alternative is standard partitioning)?"
+		echo "(0) Use standard partitioning"
+		echo "(1) Use LVM"
 		read -p "> " USE_LVM
-		if [ $USE_LVM != 0 ] && [ $USE_LVM != 1 ]; then
+		if [ $USE_LVM != 0 ] && [ $USE_LVM != 1 ]
+		then
 			echo " Invalid input: You must provide either \"0\" or \"1\""
 		fi
 	done
 
 	USE_ENCRYPTION=2
 	while [ $USE_ENCRYPTION != 0 ] && [ $USE_ENCRYPTION != 1 ]; do
-		echo "1.3.3 Would you like to encrypt your non-boot partitions/LV?
-		(0) DON'T encrypt partitions
-		(1) Encrypt partitions"
+		echo "1.3.3 Would you like to encrypt your non-boot partitions/LV?"
+		echo "(0) DON'T encrypt partitions"
+		echo "(1) Encrypt partitions"
 		read -p "> " USE_ENCRYPTION
-		if [ $USE_ENCRYPTION != 0 ] && [ $USE_ENCRYPTION != 1 ]; then
+		if [ $USE_ENCRYPTION != 0 ] && [ $USE_ENCRYPTION != 1 ]
+		then
 			echo " Invalid input: You must provide either \"0\" or \"1\""
 		fi
 	done
-
-	
 else
+	echo "hi"
 	# Deconstruct partition scheme into arguments
-
 fi
 
 
